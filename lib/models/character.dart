c47e2d24129c63d1a1291b8235a430fd4bcda561
junho <willight1@naver.com>
2025-06-11 20:02:45 +0900

    Changed character model Feat: Changed the Character Model

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,23 +1,31 @@
 class Character {
   final String id;
   final String name;
-  final String description;
-  final String imageUrl;
+  final String? profileImageUrl;
+  final String? description;
 
-  // Character 생성자
   Character({
     required this.id,
     required this.name,
-    required this.description,
-    required this.imageUrl,
+    this.profileImageUrl,
+    this.description,
   });
 
   factory Character.fromJson(Map<String, dynamic> json) {
     return Character(
       id: json['id'].toString(),
       name: json['name'],
-      imageUrl: json['avatarUrl'],
+      profileImageUrl: json['profile_image_url'],
       description: json['description'],
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'profile_image_url': profileImageUrl,
+      'description': description,
+    };
   }
 }
